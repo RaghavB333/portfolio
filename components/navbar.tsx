c06cbd7bf8f28d7d9db7569944706927054899ad
RaghavBhargava <raghavbhargava3@gmail.com>
2025-06-18 19:13:15 +0530

    Vercel deployment

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -110,7 +110,7 @@ export const Navbar = () => {
                       ? "danger"
                       : "foreground"
                 }
-                href="#"
+                href={item.href}
                 size="lg"
               >
                 {item.label}
